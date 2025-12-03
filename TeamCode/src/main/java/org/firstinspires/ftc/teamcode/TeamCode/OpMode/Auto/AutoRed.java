65c865ffea104bf56dda1e2f535c1567dce76e9c
Atlas <dragos.capraru.dc@gmail.com>
2025-12-03 12:07:01 +0200

    facut start pos -luca chiciu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/OpMode/Auto/AutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode/OpMode/Auto/AutoRed.java
@@ -19,7 +19,7 @@ public class AutoRed extends OpMode {
 
     private int pathState;
 
-    private final Pose startPose = new Pose(28.5, 128, Math.toRadians(180)); // Start Pose of our robot.
+    private final Pose startPose = new Pose(123, 123, Math.toRadians(-145)); // Start Pose of our robot.
     private final Pose scorePose = new Pose(60, 85, Math.toRadians(135)); // Scoring Pose of our robot. It is facing the goal at a 135 degree angle.
     private final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0)); // Highest (First Set) of Artifacts from the Spike Mark.
     private final Pose pickup2Pose = new Pose(43, 130, Math.toRadians(0)); // Middle (Second Set) of Artifacts from the Spike Mark.
